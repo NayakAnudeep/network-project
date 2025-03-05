1df0ac4e2b6826987fd9b2c35de33856fa71b116
duncan britt <dbru997@gmail.com>
2025-03-05 14:13:04 -0700

    second signup button linked.

--- aniTA_web/aniTA_app/static/aniTA_app/js/index.js
+++ aniTA_web/aniTA_app/static/aniTA_app/js/index.js
@@ -26,15 +26,19 @@ for (let i = 0; i < LOGIN_CLOSE_BTNS.length; i++) {
    ▗▄▄▞▘▗▄█▄▖▝▚▄▞▘▐▌  ▐▌▝▚▄▞▘▐▌ */
 
 const SIGNUP_MODAL = document.getElementById("modal-signup");
-const SIGNUP_LINK = document.querySelector('#signup-btn');
+const SIGNUP_LINKS = [document.querySelector('#signup-btn'),
+                      document.querySelector('#signup-now-btn')];
 const SIGNUP_CLOSE_BTNS = [document.querySelector('#modal-signup .close-btn'),
                            document.querySelector('#modal-signup .cancel')];
 
-SIGNUP_LINK.addEventListener('click', event => {
-    event.preventDefault();
-    LOGIN_MODAL.style.display = "none";
-    SIGNUP_MODAL.style.display = "block";
-});
+for (let i = 0; i < SIGNUP_LINKS.length; i++) {
+    SIGNUP_LINKS[i].addEventListener('click', event => {
+        event.preventDefault();
+        LOGIN_MODAL.style.display = "none";
+        SIGNUP_MODAL.style.display = "block";
+    });
+}
+
 for (let i = 0; i < SIGNUP_CLOSE_BTNS.length; i++) {
     SIGNUP_CLOSE_BTNS[i].addEventListener('click', event => {
         SIGNUP_MODAL.style.display = "none";
