3f55c9052da383e7547593383e33f56c73a358d8
duncan britt <dbru997@gmail.com>
2025-03-04 20:25:09 -0700

    Login.
    Expects a post route /app/login to handle login attempts.

---
+++ aniTA_web/aniTA_app/static/aniTA_app/js/index.js
@@ -0,0 +1,21 @@
+
+/* ▗▖    ▗▄▖  ▗▄▄▖▗▄▄▄▖▗▖  ▗▖
+   ▐▌   ▐▌ ▐▌▐▌     █  ▐▛▚▖▐▌
+   ▐▌   ▐▌ ▐▌▐▌▝▜▌  █  ▐▌ ▝▜▌
+   ▐▙▄▄▖▝▚▄▞▘▝▚▄▞▘▗▄█▄▖▐▌  ▐▌ */
+
+const LOGIN_MODAL = document.getElementById("modal-login");
+const LOGIN_LINK = document.querySelector('#login-btn');
+const LOGIN_CLOSE_BTNS = [document.querySelector('#modal-login .close-btn'),
+                          document.querySelector('#modal-login .cancel')];
+
+LOGIN_LINK.addEventListener('click', event => {
+    event.preventDefault();
+    LOGIN_MODAL.style.display = "block";
+});
+for (let i = 0; i < LOGIN_CLOSE_BTNS.length; i++) {
+    LOGIN_CLOSE_BTNS[i].addEventListener('click', event => {
+        LOGIN_MODAL.style.display = "none";
+    });
+}
+/* =========================================================================== */
