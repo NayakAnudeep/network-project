687db2008ac4772c0a5ac51e80addf46be5e3e14
duncan britt <dbru997@gmail.com>
2025-03-04 21:25:22 -0700

    Signup. (Front end)

--- aniTA_web/aniTA_app/static/aniTA_app/js/index.js
+++ aniTA_web/aniTA_app/static/aniTA_app/js/index.js
@@ -11,6 +11,7 @@ const LOGIN_CLOSE_BTNS = [document.querySelector('#modal-login .close-btn'),
 
 LOGIN_LINK.addEventListener('click', event => {
     event.preventDefault();
+    SIGNUP_MODAL.style.display = "none";
     LOGIN_MODAL.style.display = "block";
 });
 for (let i = 0; i < LOGIN_CLOSE_BTNS.length; i++) {
@@ -18,4 +19,24 @@ for (let i = 0; i < LOGIN_CLOSE_BTNS.length; i++) {
         LOGIN_MODAL.style.display = "none";
     });
 }
-/* =========================================================================== */
+
+/*  ▗▄▄▖▗▄▄▄▖ ▗▄▄▖▗▖  ▗▖▗▖ ▗▖▗▄▄▖
+   ▐▌     █  ▐▌   ▐▛▚▖▐▌▐▌ ▐▌▐▌ ▐▌
+    ▝▀▚▖  █  ▐▌▝▜▌▐▌ ▝▜▌▐▌ ▐▌▐▛▀▘
+   ▗▄▄▞▘▗▄█▄▖▝▚▄▞▘▐▌  ▐▌▝▚▄▞▘▐▌ */
+
+const SIGNUP_MODAL = document.getElementById("modal-signup");
+const SIGNUP_LINK = document.querySelector('#signup-btn');
+const SIGNUP_CLOSE_BTNS = [document.querySelector('#modal-signup .close-btn'),
+                           document.querySelector('#modal-signup .cancel')];
+
+SIGNUP_LINK.addEventListener('click', event => {
+    event.preventDefault();
+    LOGIN_MODAL.style.display = "none";
+    SIGNUP_MODAL.style.display = "block";
+});
+for (let i = 0; i < SIGNUP_CLOSE_BTNS.length; i++) {
+    SIGNUP_CLOSE_BTNS[i].addEventListener('click', event => {
+        SIGNUP_MODAL.style.display = "none";
+    });
+}
--- aniTA_web/aniTA_server/urls.py
+++ aniTA_web/aniTA_server/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('app/', include("aniTA_app.urls")),
+    path('', include("aniTA_app.urls")),
     path('admin/', admin.site.urls),
 ]
