5fa6a40a3af35edecbb61f141bd9d1731b91def6
duncan britt <dbru997@gmail.com>
2025-03-18 11:54:26 -0600

    bug fix

--- aniTA_web/aniTA_app/urls.py
+++ aniTA_web/aniTA_app/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
-    path("signup/", views.signup, name="signup"),
     path("dashboard/", views.dashboard, name="student_dashboard"),
     path("courses/", views.courses, name="all_courses"),
-    # path("<int:arg_foo>/dashboard/", views.dashboard, name="dashboard"),
     path("users/", include('users.urls')),
 ]
